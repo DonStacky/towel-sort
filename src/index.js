83fae3fde972cbcea3d8f8419eac11a466f5f523
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-01-24 10:14:25 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    const arr = [];
+    matrix.forEach((value) => {
+        arr.push(...value);
+    })
+  return arr;
 }
